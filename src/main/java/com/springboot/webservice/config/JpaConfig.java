93ef321b0d415c8fe3847e0d45aa3bdf50deac16
sw <>
2020-04-22 16:25:47 +0900

    Jpa service - OAuth2, Spring Security 적용

---
+++ src/main/java/com/springboot/webservice/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package com.springboot.webservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
--- src/test/java/com/springboot/webservice/web/HelloControllerTest.java
+++ src/test/java/com/springboot/webservice/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.springboot.webservice.web;
 
+import com.springboot.webservice.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,12 +18,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)}
+        )
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloReturn() throws Exception {
         String hello = "hello springboot";
@@ -28,6 +35,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDtoReturnTest() throws Exception {
         String name = "hello";
